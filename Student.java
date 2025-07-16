b1875cdd54494e80dc40b44474e557ba67f55ef9
vikkas yadav <144140105+Invalid07@users.noreply.github.com>
2025-07-16 17:32:12 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,29 @@
+package com.corejava;
+
+
+// pojo (plain old java object)
+public class Student 
+{
+
+//	private variable(encapsulation)
+	private String name;
+	private int age ;
+	
+//	getter and setter
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+//	public setter and getter
+	
+	
+}
